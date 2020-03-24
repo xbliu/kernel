ad5c24ce805e15c6112dad87f0052a2040488c15
xubo.liu@ankobot.com <xuboliu2018>
2020-03-24 09:48:57 +0800

    add workqueue comments 5

--- linux-4.14.18/kernel/workqueue.c
+++ linux-4.14.18/kernel/workqueue.c
@@ -1422,7 +1422,7 @@ retry:
     /*
     a4.work可能正在运行,为了确保work的callback function不会重入,该work最好是挂入此pool workqueue. 
     last_pool与选择的pwq->pool不一致,根据last pool找到对应的pool workqueue, 
-    再找到具体哪一个worker线程正在处理该work,若能找到此woker,使用它,否则还是pwq->pool 
+    再找到具体哪一个worker线程正在处理该work,若能找到此woker,使用它,否则还是pwq->pool. 
     */ 
 	last_pool = get_work_pool(work);
 	if (last_pool && last_pool != pwq->pool) {
@@ -1506,6 +1506,25 @@ retry:
  *
  * Return: %false if @work was already on a queue, %true otherwise.
  */
+/*
+同一时间无论bound还是unbound workqueue,同一work只能被系统的中一个worker处理,即任何work都是非重入的.
+1)同一时间同一work被queue多次,pending的work只会挂入一次workqueue(同一时刻只有一个pending的work)
+    queue_work_on
+    -->test_and_set_bit(WORK_STRUCT_PENDING_BIT, work_data_bits(work)
+2)不同时间的同一work多次queue,若work正在被执行,挂入上一次的worker_pool.也就是相同的worker_pool
+    __queue_work
+    --->
+        last_pool = get_work_pool(work)
+        worker = find_worker_executing_work(last_pool, work)
+        if (worker && worker->current_pwq->wq == wq) {
+			pwq = worker->current_pwq;
+		}
+3)若当前worker_pool有一worker正在执行同一work,将其挂入此worker
+    process_one_work
+    --->
+        collision = find_worker_executing_work(pool, work)
+同一时间同一work至多只有一个pending与一个正在执行中.
+*/
 bool queue_work_on(int cpu, struct workqueue_struct *wq,
 		   struct work_struct *work)
 {
@@ -2103,6 +2122,7 @@ __acquires(&pool->lock)
 	 * currently executing one.
 	 */
     /* 
+      单个work不应由单个cpu 上的多个worker同时执行(阻止了单个work的并发).
       a1.查找此work是否已由其它worker上处理.
       根椐cache特性,即然已有其它worker处理,当然继续由此worker处理更快.
     */
